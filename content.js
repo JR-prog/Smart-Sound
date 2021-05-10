936df4aad80ad64781cbdd0f7d1e4badaecb7feb
JR-prog <roebuck.jayc@gmail.com>
2021-05-10 19:24:11 -0400

    content.js now sends bgm tab id to background

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-alert('testttt');
+// This only runs when the popup button is pressed
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // Send bgm tab id to background
+    chrome.runtime.sendMessage({
+        id: request
+    });
+});
